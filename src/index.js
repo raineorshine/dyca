dd5f8d90800e0d888aa8f4c6315cb9f23f541a86
Raine Revere <raine@cybersemics.org>
2021-02-05 10:59:13 -0700

    ad hoc collections (1 level)

--- src/index.js
+++ src/index.js
@@ -1,19 +1,39 @@
 const uid = require('uuid').v4
 
 const dyca = db => {
+
+  const collections = {}
+
   const store = {
 
     set: (path, value) => {
       if (value === undefined) {
         value = path
-        path = uid()
+        path = ''
       }
-      db.set(path, value)
-      return path
+
+      // set in db
+      const id = uid()
+      db.set(id, value)
+
+      // index
+      if (!collections[path]) {
+        collections[path] = new Set()
+      }
+      collections[path].add(id)
+
+      return id
     },
 
     get: path => {
       const parts = path.split('/')
+
+      if (collections[parts[0]]) {
+        return Array.from(collections[parts[0]].keys())
+          .map(key => db.get(key))
+      }
+
+      const collection = collections[parts[0]] || db
       const value = parts.slice(1).reduce((accum, part) => accum[part], db.get(parts[0]))
       return value
     },
--- src/index.test.js
+++ src/index.test.js
@@ -6,18 +6,19 @@ test('auto key', () => {
   expect(store.get(id)).toBe(1)
 })
 
-test('key-value', () => {
+test('ad hoc collection (1 level)', () => {
   const store = dyca(new Map())
-  store.set('a', 1)
-  expect(store.get('a')).toBe(1)
+  const id = store.set('a', 1)
+  expect(store.get(id)).toEqual(1)
+  expect(store.get('a')).toEqual([1])
 })
 
-test('get nested property via path', () => {
-  const store = dyca(new Map())
-  store.set('foo', {
-    bar: {
-      baz: 1
-    }
-  })
-  expect(store.get('foo/bar/baz')).toBe(1)
-})
+// test('get nested property via path', () => {
+//   const store = dyca(new Map())
+//   store.set('foo', {
+//     bar: {
+//       baz: 1
+//     }
+//   })
+//   expect(store.get('foo/bar/baz')).toBe(1)
+// })
